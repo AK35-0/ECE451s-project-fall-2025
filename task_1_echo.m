43e2fff683308d99c608390efb34d787838456b1
Amr Khaled <167694281+AK35-0@users.noreply.github.com>
2025-11-24 23:15:19 +0200

    Refactor audio processing parameters and MSE calculations
    Updated variable names for consistency and improved MSE calculation precision.

--- task_1_echo.m
+++ task_1_echo.m
@@ -7,7 +7,7 @@ clc; clear; close all;
 
 try
     % Load x_n (audio signal), N (length), and Fs (sampling frequency)
-    load('project_parameters.mat', 'x_n', 'X_k', 'N', 'n', 'f', 'w', 'fs');
+    load('project_parameters.mat', 'x_n', 'X_k', 'N', 'n', 'f', 'w', 'Fs', 'Energy_n');
 catch
     disp('ERROR: project_parameters.mat not found. Please run task_0_audio.m first.');
     return;
@@ -30,9 +30,9 @@ plt_sys(b_h, a_h, N, n, f, w, 'Echo System H(z)');
 
 % Apply H(z) to audio x[n]
 Y1 = X_k .* H_z;
-y1 = ifft(Y1) .* fs;
-MSE_y1 = 100 * sum((y1 - x_n).^2)/(N+1);
-fprintf('MSE_y1 (Echo Signal vs Original): %.2f\n', MSE_y1);
+y1 = ifft(Y1) * Fs;
+MSE_y1 = sum((y1 - x_n).^2)/(N+1);
+fprintf('MSE_y1 (Echo Signal vs Original): %.3e\n', MSE_y1);
 
 %% 2. Equalizer System G(z)
 
@@ -46,8 +46,8 @@ plt_sys(b_g, a_g, N, n, f, w, 'Equalizer System G(z)');
 
 % Apply G(z) to echoed signal y1[n]
 Y2 = Y1 .* G_z;
-y2 = ifft(Y2).*fs;
-MSE_y2 = 100 * sum((y2 - x_n).^2)/(N+1);
+y2 = ifft(Y2) * Fs;
+MSE_y2 = sum((y2 - x_n).^2)/(N+1);
 fprintf('MSE_y2 (Equalized Signal vs Original): %.2f\n', MSE_y2);
 fprintf('----------------------------------\n');
 
