486644e305d779238494f6a6d5f370a53eb27921
MrMystery <113464087+crash1218@users.noreply.github.com>
2025-12-07 14:37:45 +0200

    Jiraya Sensei Fixed the BPF Frequency response not plotting

--- task_3_transform.m
+++ task_3_transform.m
@@ -7,6 +7,15 @@ catch
     return;
 end
 
+try
+    % Load x_n (audio signal), N (length), and Fs (sampling frequency)
+    load('project_parameters.mat', 'x_n', 'X_k', 'N', 'n', 'f', 'w', 'Fs', 'Energy_n');
+catch
+    disp('ERROR: project_parameters.mat not found. Please run task_0_audio.m first.');
+    return;
+end
+
+filter_types = {'HPF Filter', 'BPF Filter'};
 % 1. Get the order (length) of the coefficients
 Nb = length(b_butt);
 Na = length(a_butt);
@@ -28,51 +37,34 @@ a_hp = a_butt .* sign_a;
 
 % 4. Plot the original LPF and the new HPF systems for comparison
 
-figure;
-subplot(2,1,1);
-zplane(b_butt, a_butt);
-title('Original LPF (Butterworth) Pole-Zero Plot');
-grid on;
-
-subplot(2,1,2);
-zplane(b_hp, a_hp);
-title('Transformed HPF Pole-Zero Plot (Rotation by 180^{\circ})');
-grid on;
-
-% Optional: Plot the frequency responses to confirm the transformation
-figure;
-freqz(b_butt, a_butt, 512, Fs);
-hold on;
-freqz(b_hp, a_hp, 512, Fs);
-legend('LPF', 'HPF');
-title('Frequency Response Comparison');
+analyze_filter(b_hp, a_hp, x_n, Energy_n, N, f, w, 'hpf');
 
 % BPF
+[z_lpf, p_lpf, k_lpf] = tf2zp(b_butt, a_butt);
 
-theta = pi/2;
+% Transform: z -> -z
+z_hpf = -z_lpf;
+p_hpf = -p_lpf;
 
-% 3. Apply the LPF-to-HPF transformation by element-wise multiplication
-b_1 = b_butt .* (exp(1j .* nb.* theta));
-a_1 = a_butt .* (exp(1j .* na.* theta));
+% Scale the gain
+k_hpf = k_lpf * real(prod(1-p_lpf)/prod(1-z_lpf));
 
-a_bp = conv(a_1, conj(a_1));
-b_bp = conv(b_butt, b_hp);
+% --- Convert HPF to (b, a) Form ---
+[b_hpf, a_hpf] = zp2tf(z_hpf, p_hpf, k_lpf);
 
-figure;
+%% BPF
+[z_lpf, p_lpf, k_lpf] = tf2zp(b_butt, a_butt);
 
-subplot(3,1,1);
-zplane(b_butt, a_butt);
-title('Original LPF (Butterworth) Pole-Zero Plot');
-grid on;
+z_bpf = [z_lpf ; -z_lpf];
+p_bpf = [p_lpf * 1i ; p_lpf * -1i];
 
-subplot(2,1,2);
-zplane(b_bp, a_bp);
-title('Transformed BPF Pole-Zero Plot (Rotation by 90^{\circ})');
-grid on;
-% Optional: Plot the frequency responses to confirm the transformation
-figure;
-freqz(b_butt, a_butt, 512, Fs);
-hold on;
-freqz(b_bp, a_bp, 512, Fs);
-legend('LPF', 'BPF');
-title('Frequency Response Comparison');
+% Scale the gain
+[b_raw, a_raw] = zp2tf(z_bpf, p_bpf, 1);
+[H_raw, f_raw] = freqz(b_raw, a_raw, 4096, Fs);
+idx = (f_raw >= 12e3) & (f_raw <= 15e3);
+k_bpf = 1/mean(abs(H_raw(idx)));
+
+% --- Convert BPF to (b, a) Form ---
+[b_bp, a_bp] = zp2tf(z_bpf, p_bpf, k_bpf);
+
+analyze_filter(b_bp, a_bp, x_n, Energy_n, N, f, w, 'BPF');
