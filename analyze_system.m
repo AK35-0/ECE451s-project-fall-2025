b86e3867cb1fbbbd1ccb3088cdadb1f7e009ec9f
Amr Khaled <167694281+AK35-0@users.noreply.github.com>
2025-12-07 18:45:12 +0200

    Update and rename plt_sys.m to analyze_system.m

---
+++ analyze_system.m
@@ -0,0 +1,60 @@
+function [mse, energy_loss] = analyze_system(b, a, x_n, energy_x, N, fs, filter_name)
+    % This function generates all plots and computes MSE/Energy Loss
+
+    % 1. Pole-Zero Plot
+    figure('Name', [filter_name ': Pole-Zero Plot']);
+    zplane(b, a);
+    title(['Pole-Zero Plot for ', filter_name]);
+    grid on;
+
+    % 2. Magnitude and Phase Response
+    [H, w] = freqz(b, a, N, 'whole');
+    w = w - pi; f = w/pi * fs/2;
+    phi = unwrap(angle(fftshift(H)));
+    figure('Name', [filter_name ': Frequency Response']);
+    
+    % Magnitude
+    subplot(2,1,1);
+    plot(f, 20*log10(abs(fftshift(H))));
+    title(['Magnitude Response for ', filter_name]);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
+    grid on; xlim([-100, 100]);
+    
+    % Phase Response
+    subplot(2,1,2);
+    plot(f, rad2deg(phi));
+    title(['Phase Response for ', filter_name]);
+    xlabel('Frequency (Hz)');
+    ylabel('Phase (degrees)');
+    grid on; xlim([-100, 100]);
+
+    % 3. Group Delay
+    gd = grpdelay(b, a, N, 'whole');
+    figure('Name', [filter_name ': Group Delay']);
+    plot(w/pi, fftshift(gd));
+    title(['Group Delay for ', filter_name]);
+    xlabel('Normalized Angular Frequency (\pi rad/s)');
+    ylabel('Group Delay (samples)');
+    grid on; xlim([-3 3]*1e-3);
+
+    % 4. Impulse Response
+    figure('Name', [filter_name ': Impulse Response']);
+    impz(b, a);
+    title(['Impulse Response for ', filter_name]);
+    grid on;
+    
+    % Check if the input is available
+    if ~isempty(x_n) && (numel(x_n) > 1)
+        % MSE
+        y = filter(b, a, x_n);  % Apply filter to signal
+        mse = sum((y - x_n).^2)/(length(x_n)+1);
+
+        % Energy Loss
+        energy_y = sum(abs(y).^2);
+        energy_loss = energy_x - energy_y;
+        energy_loss = (energy_loss / energy_x) * 100;
+    else
+        fprintf('Skipping MSE and Energy Loss calculation (No input signal provided).\n');
+    end
+end
